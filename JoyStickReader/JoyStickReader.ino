8615f799cb6100d8e81ada48e62b9f39a5101c53
Unknown <cihankurt1998@gmail.com>
2018-08-31 15:57:10 +0200

    RawData
    This version gets the raw data, so numbers from 0 to 1024. The code is only for the joystick. The click button doesn't seem the work 100% right, but that's the wiring in my case.

---
+++ JoyStickReader/JoyStickReader.ino
@@ -0,0 +1,35 @@
+int YaxisValue = 0;
+int XaxisValue = 0;
+int YaxisPin = A1;
+int XaxisPin = A0;
+int ClickPin = 2;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ClickPin, INPUT);
+}
+
+void loop()
+{
+ ReadValues();
+ PrintValues();
+}
+
+void ReadValues()
+{
+  YaxisValue = analogRead(YaxisPin);
+  XaxisValue = analogRead(XaxisPin);
+}
+
+void PrintValues()
+{
+  Serial.print("Y axis: ");
+  Serial.print(YaxisValue);
+  Serial.print("     X axis: ");
+  Serial.print(XaxisValue);
+  Serial.print("     ClickStatus: ");
+  Serial.println(digitalRead(ClickPin));
+}
+
